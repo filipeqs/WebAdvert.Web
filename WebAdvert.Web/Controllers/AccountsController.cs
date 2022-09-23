31e7659c352e9609fc64b65dce2bdb6a6847c645
Filipe Silva <filipeqs@outlook.com>
2022-09-22 21:14:04 -0400

    login and create user using aws congnito

---
+++ WebAdvert.Web/Controllers/AccountsController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using WebAdvert.Web.Models.Accounts;
+using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Amazon.CognitoIdentityProvider;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+
+        public AccountsController(
+            SignInManager<CognitoUser> signInManager,
+            UserManager<CognitoUser> userManager,
+            CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Signup()
+        {
+            var model = new SignupModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists");
+                    return View(model);
+                }
+
+                user.Attributes.Add(CognitoAttribute.Name.AttributeName, model.Email);
+                var createdUser = await _userManager.CreateAsync(user, model.Password);
+
+                if (createdUser.Succeeded)
+                    RedirectToAction("Confirm");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [ActionName("Confirm")]
+        public async Task<IActionResult> Confirm()
+        {
+            var model = new ConfirmModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Confirm")]
+        public async Task<IActionResult> Confirm_Post(ConfirmModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.Email);
+                if (user == null)
+                {
+                    ModelState.AddModelError("NotFound", "A user with the given address was not found");
+                    return View(model);
+                }
+
+                var result = await (_userManager as CognitoUserManager<CognitoUser>).ConfirmSignUpAsync(user, model.Code, true).ConfigureAwait(false);
+                if (result.Succeeded)
+                    return RedirectToAction("Index", "Home");
+                else
+                {
+                    foreach (var error in result.Errors)
+                        ModelState.AddModelError(error.Code, error.Description);
+
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            var model = new LoginModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                    return RedirectToAction("Index", "Home");
+                else
+                    ModelState.AddModelError("LoginError", "Email and password do not match");
+            }
+
+            return View(model);
+        }
+    }
+}
